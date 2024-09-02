3d7901379758aebd06d2a0d2ab45eff329af215d
jiseon <mongbu54@gmail.com>
2024-09-02 22:58:56 +0900

    ITDS-38 feat: 로그인 유저 객체를 받아오는 custom annotation 적용

--- src/main/java/com/dissonance/itit/controller/FeaturedPostContorller.java
+++ src/main/java/com/dissonance/itit/controller/FeaturedPostContorller.java
@@ -1,26 +1,28 @@
 package com.dissonance.itit.controller;
 
-import com.dissonance.itit.dto.response.FeaturedPostRes;
-import com.dissonance.itit.service.FeaturedPostService;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.dissonance.itit.dto.response.FeaturedPostRes;
+import com.dissonance.itit.service.FeaturedPostService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("featured-posts")
 public class FeaturedPostContorller {
-    private final FeaturedPostService featuredPostService;
+	private final FeaturedPostService featuredPostService;
 
-    @GetMapping
-    @Operation(summary = "추천 게시글 조회", description = "운영진 추천 게시글 5개를 조회합니다.")
-    public ResponseEntity<List<FeaturedPostRes>> getFeaturedPosts() {
-        List<FeaturedPostRes> featuredPostRes = featuredPostService.getFeaturedPost();
-        return ResponseEntity.ok(featuredPostRes);
-    }
+	@GetMapping
+	@Operation(summary = "추천 게시글 조회", description = "운영진 추천 게시글 5개를 조회합니다.")
+	public ResponseEntity<List<FeaturedPostRes>> getFeaturedPosts() {
+		List<FeaturedPostRes> featuredPostRes = featuredPostService.getFeaturedPost();
+		return ResponseEntity.ok(featuredPostRes);
+	}
 }
