43c01e10096c118310ec3f6545679b5778d2e5a6
jiseon <mongbu54@gmail.com>
2024-09-09 02:01:08 +0900

    ITDS-25 feat: provider 로그인 분기 처리

--- src/main/java/com/dissonance/itit/service/UserService.java
+++ src/main/java/com/dissonance/itit/service/UserService.java
@@ -8,6 +8,7 @@ import com.dissonance.itit.common.exception.ErrorCode;
 import com.dissonance.itit.common.jwt.util.JwtUtil;
 import com.dissonance.itit.domain.entity.User;
 import com.dissonance.itit.domain.enums.Role;
+import com.dissonance.itit.domain.enums.SocialLoginProvider;
 import com.dissonance.itit.dto.response.GeneratedToken;
 import com.dissonance.itit.dto.response.LoginUserInfoRes;
 import com.dissonance.itit.dto.response.OAuthUserInformation;
@@ -20,16 +21,21 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Service
 public class UserService {
-	private final OauthService oauthService;
-	private final UserRepository userRepository;
 	private final JwtUtil jwtUtil;
+	private final UserRepository userRepository;
+
+	private final KakaoOAuthServiceImpl kakaoOAuthService;
+	private final AppleOAuthServiceImpl appleOAuthService;
 
 	@Transactional
 	public GeneratedToken login(String provider, String token) {
 		OAuthUserInformation userInformation;
+		SocialLoginProvider providerEnum = SocialLoginProvider.valueOf(provider.toUpperCase());
 
-		if (provider.equals("kakao")) {
-			userInformation = oauthService.requestUserInformation(token);
+		if (providerEnum.equals(SocialLoginProvider.APPLE)) {
+			userInformation = appleOAuthService.requestUserInformation(token);
+		} else if (providerEnum.equals(SocialLoginProvider.KAKAO)) {
+			userInformation = kakaoOAuthService.requestUserInformation(token);
 		} else {
 			log.info("존재하지 않는 provider: " + provider);
 			throw new CustomException(ErrorCode.INVALID_PROVIDER);
@@ -37,7 +43,7 @@ public class UserService {
 
 		User user;
 
-		if (userRepository.existsByProviderAndProviderId(provider, userInformation.getProviderId())) {
+		if (isExistsByProviderAndProviderId(providerEnum, userInformation.getProviderId())) {
 			log.info("[UserService] login, response: {}", userInformation);
 
 			user = findByEmail(userInformation.getEmail());
@@ -45,25 +51,34 @@ public class UserService {
 		} else {
 			log.info("[UserService] signUp, response: {}", userInformation);
 
-			user = User.builder()
-				.email(userInformation.getEmail())
-				.password("kakao")
-				.name(userInformation.getNickname())
-				.provider(userInformation.getProvider())
-				.providerId(userInformation.getProviderId())
-				.profileImgUrl(userInformation.getProfileImgUrl())
-				.role(Role.USER)
-				.build();
-
-			userRepository.save(user);
+			user = saveUser(userInformation);
 		}
 
 		return jwtUtil.generateToken(user.getEmail(), user.getRole().getKey());
 	}
 
+	private boolean isExistsByProviderAndProviderId(SocialLoginProvider provider, String providerId) {
+		return userRepository.existsByProviderAndProviderId(provider, providerId);
+	}
+
 	private User findByEmail(String email) {
 		return userRepository.findByEmail(email)
 			.orElseThrow(() -> new CustomException(ErrorCode.NON_EXISTENT_EMAIL));
+	}
+
+	private User saveUser(OAuthUserInformation userInformation) {
+		User user = User.builder()
+			.email(userInformation.getEmail())
+			.name(userInformation.getNickname())
+			.provider(userInformation.getProvider())
+			.providerId(userInformation.getProviderId())
+			.profileImgUrl(userInformation.getProfileImgUrl())
+			.role(Role.USER)
+			.build();
+
+		userRepository.save(user);
+
+		return user;
 	}
 
 	@Transactional(readOnly = true)
