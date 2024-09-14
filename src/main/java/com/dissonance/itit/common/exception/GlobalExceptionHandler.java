bd066c8084f7d21f841b9bb914fa3e2163c4381d
jiseon <mongbu54@gmail.com>
2024-09-14 16:36:55 +0900

    ITDS-45 feat: 예외처리 공통 응답 객체 반영

--- src/main/java/com/dissonance/itit/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dissonance/itit/common/exception/GlobalExceptionHandler.java
@@ -5,22 +5,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.dissonance.itit.common.util.ApiResponse;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(CustomException.class)
-	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-		ErrorResponse response = new ErrorResponse(e.getErrorCode().name(), e.getMessage());
+	protected ResponseEntity<?> handleCustomException(CustomException e) {
+		int statusCode = e.getErrorCode().getHttpStatus().value();
 		log.error("CustomException : {}", e.getMessage());
-		return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+		return new ResponseEntity<>(ApiResponse.error(statusCode, e.getMessage()),
+			e.getErrorCode().getHttpStatus());
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Object> handleAllException(final Exception e) {
-		ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.name(), e.getMessage());
+	public ResponseEntity<?> handleAllException(final Exception e) {
+		int statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
 		log.error("handleAllException {}", e.getMessage());
-		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<>(ApiResponse.error(statusCode, e.getMessage()),
+			HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
