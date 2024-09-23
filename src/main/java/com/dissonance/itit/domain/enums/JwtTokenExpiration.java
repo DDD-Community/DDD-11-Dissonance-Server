c3a1d6c69e59b3298c734e1a3db0542d14916bd5
jiseon <mongbu54@gmail.com>
2024-09-23 22:08:05 +0900

    fix: 프론트 테스트용 token 임시 연장

--- src/main/java/com/dissonance/itit/domain/enums/JwtTokenExpiration.java
+++ src/main/java/com/dissonance/itit/domain/enums/JwtTokenExpiration.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum JwtTokenExpiration {
-	ACCESS_TOKEN_EXPIRED_TIME("1시간", 1000L * 60 * 60),
+	ACCESS_TOKEN_EXPIRED_TIME("1시간", 1000L * 60 * 60 * 1000), // FIXME: 테스트용 임시 연장
 	REFRESH_TOKEN_EXPIRATION_TIME("2주", 1000L * 60 * 60 * 24 * 14);
 
 	private final String description;
