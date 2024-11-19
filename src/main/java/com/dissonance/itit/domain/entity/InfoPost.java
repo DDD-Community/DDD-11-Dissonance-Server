4c4afd2f56a1640614c161ee15ba6a65fb5b209b
jiseon <mongbu54@gmail.com>
2024-11-19 15:52:09 +0900

    ITDS-63 fix: 모집 마감일 not null 설정

--- src/main/java/com/dissonance/itit/domain/entity/InfoPost.java
+++ src/main/java/com/dissonance/itit/domain/entity/InfoPost.java
@@ -52,6 +52,7 @@ public class InfoPost extends BaseTime {
 	@Column(name = "recruitment_start_date")
 	private LocalDate recruitmentStartDate;
 
+	@NotNull
 	@Column(name = "recruitment_end_date")
 	private LocalDate recruitmentEndDate;
 
