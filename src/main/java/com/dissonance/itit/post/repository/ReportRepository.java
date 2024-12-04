63432f93711a3de10b72c0089979ad6259db3989
jiseon <mongbu54@gmail.com>
2024-12-04 15:50:02 +0900

    feat: 신고 기능 기획 변경으로 report domain 삭제

--- src/main/java/com/dissonance/itit/post/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dissonance.itit.post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dissonance.itit.post.domain.Report;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-	boolean existsByInfoPostIdAndUserId(Long infoPostId, Long UserId);
-}
